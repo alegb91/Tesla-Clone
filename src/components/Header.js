95750f3436d00beac409d4e5ffcd76347082f3fa
Alejandro Gonzalez Buffo <alegb91@gmail.com>
2022-07-15 12:59:00 -0400

    Segunda Version

---
+++ src/components/Header.js
@@ -0,0 +1,128 @@
+import React, { useState } from 'react'
+import styled from 'styled-components';
+import { Menu, Close } from '@material-ui/icons';
+
+const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px 20px;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+`
+
+const MiddleMenu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 0px 10px;
+        flex-wrap: nowrap;
+    }
+
+    @media (max-width: 768px) {
+        display: none;
+    }
+`
+
+const RightMenu = styled.div`
+        display: flex;
+        align-items: center;
+
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        margin-right: 10px;
+    }
+`
+
+const CustomMenu = styled(Menu)`
+    cursor: pointer;
+`
+
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 10;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transform: ${props => props.show ? "translateX(0)" : "translateX(100%)"};
+    transition: transform 0.2s ease-in;
+    
+    li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, .2);
+
+        a {
+            font-weight: 600;
+
+        }
+    }
+`
+
+const CloseWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`
+
+const CustomClose = styled(Close)`
+    cursor: pointer;
+`
+
+
+
+const Header = () => {
+
+    const [burgerStatus, setBurgerStatus ] = useState(false);
+
+
+  return (
+    <Container>
+       <a>
+            <img src="/images/logo.svg" alt="" />
+       </a>
+       <MiddleMenu>
+       <a href="#">Model S</a>
+       <a href="#">Model 3</a>
+       <a href="#">Model X</a>
+       <a href="#">Model Y</a>
+       </MiddleMenu>
+       <RightMenu>
+            <a href="#">Shop</a>
+            <a href="#">Tesla Account</a>
+            <CustomMenu onClick={() => setBurgerStatus(true)}/>
+       </RightMenu>
+       <BurgerNav show={burgerStatus}>
+            <CloseWrapper>
+                <CustomClose onClick={() => setBurgerStatus(false)}/>
+            </CloseWrapper>   
+                <li><a href="#">Model S</a></li>
+                <li><a href="#">Model 3</a></li>
+                <li><a href="#">Model X</a></li>
+                <li><a href="#">Model Y</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadster</a></li>
+       </BurgerNav>
+    </Container>
+  )
+}
+
+export default Header;
---
+++ src/components/Section.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Fade } from "react-reveal";
+
+const Wrap = styled.div`
+width: 100vw;
+height: 100vh;
+background-size: cover;
+background-repeat: no-repeat;
+background-image: url("/images/${props => props.bgImage}");
+background-position: center;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+align-items: center;
+`
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+
+const Buttons = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px; 
+    cursor: pointer;
+    margin: 8px;
+`
+
+const RightButton = styled(LeftButton)`
+background: white;
+opacity: 0.65;
+color: black;
+`
+
+const DownArrow = styled.img`
+height: 40px;
+animation: animateDown infinite 1.5s;
+overflow-x: hidden;
+`
+
+const Section = (props) => {
+    console.log(props)
+  return (
+    <Wrap bgImage={props.backgroundImg}>
+        <Fade bottom>
+            <ItemText>
+                <h1>{ props.title }</h1>
+                <p>{ props.description }</p>
+            </ItemText>
+        </Fade>
+        <Buttons>
+        <Fade bottom>
+            <ButtonGroup>
+                <LeftButton>
+                    { props.leftBtnText }
+                </LeftButton>
+                {props.rightBtnText &&
+                <RightButton>
+                { props.rightBtnText }
+                </RightButton>
+                }
+                
+            </ButtonGroup>
+        </Fade>
+        <DownArrow src="/images/down-arrow.svg" />
+        </Buttons>
+    </Wrap>
+  )
+}
+
+export default Section;
